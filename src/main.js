fa5ed1d89514cb825ac595709c8807464774087f
Zui <zuik@users.noreply.github.com>
2021-04-17 23:22:38 -0400

    Misc

--- src/main.js
+++ src/main.js
@@ -1,20 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import VueCarousel from "vue-carousel";
 
 // Import Bootstrap an BootstrapVue CSS files (order is important)
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+Vue.use(VueCarousel);
 
-Vue.use(BootstrapVue)
-Vue.use(IconsPlugin)
-
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
