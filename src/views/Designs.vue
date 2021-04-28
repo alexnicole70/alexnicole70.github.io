ef048cb8d48a9436ae7b386d2b4a1f091f9cf229
Zui <zuik@users.noreply.github.com>
2021-04-27 22:20:44 -0400

    New Design View

---
+++ src/views/Designs.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <design-page />
+  </div>
+</template>
+
+<script>
+import DesignPage from "../components/DesignPage.vue";
+export default {
+  components: { DesignPage },
+};
+</script>
+
+<style>
+</style>
