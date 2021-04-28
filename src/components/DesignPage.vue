0e0088a7b7876adbfead6c71754c3986ed3160cb
Zui <zuik@users.noreply.github.com>
2021-04-27 22:23:47 -0400

    New Design page

---
+++ src/components/DesignPage.vue
@@ -0,0 +1,476 @@
+<template>
+  <div class="container-fluid design-page">
+    <h1>My Designs</h1>
+    <div class="card-container d-flex flex-column">
+      <div class="d-flex flex-row card-row">
+        <div class="kard d-flex flex-row" id="card-1">
+          <div class="kard-images d-flex flex-column">
+            <img
+              src="../assets/designs/card-1/shibuya-after.jpg"
+              alt="Shibuya after"
+              class="img-fluid"
+            />
+            <img
+              src="../assets/designs/card-1/shibuya-before.jpg"
+              alt="Shibuya before"
+              class="img-fluid"
+            />
+          </div>
+          <div class="kard-text">
+            <h6>The Colours of Shibuya Crossing</h6>
+            <p>
+              Japan is a vibrant and bustling city, so I really wanted to being
+              out its liveliness by exaggerting the shades of colour. I was
+              going for the anime-style look which is also akin to.
+            </p>
+          </div>
+        </div>
+
+        <div class="kard d-flex flex-row" id="card-2">
+          <div class="kard-images d-flex flex-column">
+            <img
+              src="../assets/designs/card-2/paper-cutout.png"
+              alt="Paper cutout"
+              class="img-fluid"
+            />
+          </div>
+          <div class="kard-text">
+            <h6>Papercut Effect</h6>
+            <p>Done on Illustrator by stacking layers of colour.</p>
+          </div>
+        </div>
+      </div>
+      <div class="d-flex flex-row card-row">
+        <div class="kard d-flex flex-column" id="card-3">
+          <div class="kard-images d-flex flex-row">
+            <img
+              src="../assets/designs/card-3/theater-after.jpg"
+              class="img-fluid"
+              alt="Theatre image after"
+            />
+            <img
+              src="../assets/designs/card-3/theater-before.jpg"
+              class="img-fluid"
+              alt="Theatre image before"
+            />
+          </div>
+          <div class="kard-text">
+            <h6>Theatre Lights</h6>
+            <p>
+              My boyfriend and I watched our first performance of The Book of
+              Mormon at the London West End. It was a spectacle to behold.
+            </p>
+          </div>
+        </div>
+        <div class="kard d-flex flex-column" id="card-4">
+          <div class="kard-images d-flex flex-column">
+            <img
+              src="../assets/designs/card-4/conjuring.jpg"
+              alt="Conjuring"
+              class="img-fluid"
+            />
+          </div>
+          <div class="kard-text">
+            <h6>Conjuring</h6>
+            <p>
+              Surrounded by mythical smoke, this girl looks appears to be
+              conjuring dark magic by the sunset.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="card-row">
+        <div class="kard block" id="card-5">
+          <div class="kard-images d-flex flex-row background">
+            <img
+              src="../assets/designs/card-5/chikorita.png"
+              alt="Chikorita"
+              class="card-img"
+              style="padding:0"
+            />
+          </div>
+          <div class="overlay">
+            <div class="d-flex flex-row">
+              <div class="kard-text">
+                <h6>The Chikorita Family</h6>
+                <p>
+                  Chikorita and its evolutionary line have been my favourite
+                  Pokémon since I was young. I wanted to bring out their playful
+                  and nuturing tendecies with this design. P.s. Can you spot the
+                  tiny creatures hiding in the grass land?
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="d-flex flex-row card-row">
+        <div class="d-flex flex-column" style="width: 50%;">
+          <div class="kard d-flex flex-row" id="card-6">
+            <div class="kard-images d-flex flex-row">
+              <img
+                src="../assets/designs/card-6/vines.jpg"
+                alt="Vines"
+                class="img-fluid"
+              />
+            </div>
+            <div class="kard-text">
+              <h6>Vines</h6>
+              <p>
+                An interlacing of images that highlight the word’s meaning.
+              </p>
+            </div>
+          </div>
+          <div class="kard d-flex flex-row" id="card-7b">
+            <div class="kard-images d-flex flex-row">
+              <img
+                src="../assets/designs/card-7b/balloon.jpg"
+                alt="Vines"
+                class="img-fluid"
+              />
+            </div>
+            <div class="kard-text">
+              <h6>Balloon Dog</h6>
+              <p>
+                A fun play of light and shadow to fake the 3D effect using
+                Photoshop.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="kard d-flex flex-column" id="card-8">
+          <div class="kard-images d-flex flex-row">
+            <img
+              src="../assets/designs/card-8/katsu-curry-after.jpg"
+              alt="Katsu Curry after"
+              class="img-fluid"
+            />
+            <img
+              src="../assets/designs/card-8/katsu-curry-before.jpg"
+              alt="Katsu Curry before"
+              class="img-fluid"
+            />
+          </div>
+          <div class="kard-text">
+            <h6>Food editing</h6>
+            <p>
+              One of my favourite things to edit is food, espeically since I
+              love cooking. In my edits, I love restoring the richness of its
+              colour to make the food look appealing and delectable.
+            </p>
+          </div>
+          <div class="kard-images d-flex flex-row">
+            <img
+              src="../assets/designs/card-8/roti-after.jpg"
+              alt="Roti after"
+              class="img-fluid"
+            />
+            <img
+              src="../assets/designs/card-8/roti-before.jpg"
+              alt="Roti before"
+              class="img-fluid"
+            />
+          </div>
+        </div>
+      </div>
+      <div class="card-row">
+        <div class="kard block" id="card-food">
+          <div class="kard-images d-flex flex-row background">
+            <img
+              src="../assets/designs/card-food/card-food-all.png"
+              alt="All the foods"
+              class="card-img"
+              style="padding:0"
+            />
+          </div>
+          <div class="overlay">
+            <div class="d-flex flex-row">
+              <div class="kard-text">
+                <h6>Watercolours</h6>
+                <p>
+                  A re-interpretation of my favourite food in watercolours.
+                  These are some of the dishes that I loved to order at a
+                  popular Japanese-Western fusion cafe back in Singapore.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="d-flex flex-row card-row">
+        <div class="kard d-flex flex-column" id="card-10">
+          <div class="kard-images d-flex flex-column">
+            <img
+              src="../assets/designs/card-10/bottle.png"
+              class="img-fluid"
+              alt="Pokemon Terrarium"
+            />
+          </div>
+          <div class="kard-text">
+            <h6>Pokemon Terrarium</h6>
+            <p>
+              Inspired by my favourite childhood anime, an opposition of
+              environments that portray the eternal strife of these legendary
+              birds.
+            </p>
+          </div>
+        </div>
+        <div class="kard d-flex flex-column" id="card-12">
+          <div class="kard-images d-flex flex-column">
+            <img
+              src="../assets/designs/card-12/camouflage.jpg"
+              alt="Camouflage"
+              class="img-fluid"
+            />
+          </div>
+          <div class="kard-text">
+            <h6>Camouflage</h6>
+            <p>
+              My cats used to like to hide in trees when I was young. They
+              almost seem to blend in...
+            </p>
+          </div>
+        </div>
+        <div class="kard d-flex flex-column" id="card-13">
+          <div class="kard-images d-flex flex-column">
+            <img
+              src="../assets/designs/card-13/poster.jpg"
+              alt="Summer Smoothie Splash poster"
+              class="img-fluid"
+            />
+          </div>
+          <div class="kard-text">
+            <h6>Summer Smoothie Splash</h6>
+            <p>
+              A playful combination of elements that highlight the refreshing
+              and youthful nature of the beverage.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="d-flex flex-row card-row">
+        <div class="kard d-flex flex-column" id="card-11">
+          <div class="kard-images d-flex flex-column">
+            <img
+              src="../assets/designs/card-11/boat-ride.png"
+              alt="Boat Ride"
+              class="img-fluid"
+            />
+          </div>
+          <div class="kard-text">
+            <h6>Boat Ride</h6>
+
+            <p>
+              A father cat talking to his daughter as they sail under the stars.
+              Done in the style of a cardboard cutout.
+            </p>
+          </div>
+        </div>
+        <div class="kard d-flex flex-column" id="card-14">
+          <div class="kard-images d-flex flex-column">
+            <img
+              src="../assets/designs/card-14/cat.png"
+              alt="Procreate Cat painting"
+              class="img-fluid"
+            />
+          </div>
+          <div class="kard-text">
+            <h6>Playfulness</h6>
+            <p>
+              One of my first attempts of painting on Procreate. It looks like
+              an in-between of a kitten and a squirrel.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+<style lang="scss" scoped>
+$bg-pink: #f07382;
+$bg-white: #fff8ef;
+$bg-yellow: #f7e999;
+$bg-green: #65d6d1;
+.design-page {
+  background-color: $bg-pink;
+  text-align: center;
+  h6 {
+    font-size: 1vw;
+  }
+  p {
+    font-size: 1vw;
+  }
+}
+.card-container {
+  margin: 0 7.5em;
+  .card-row {
+    justify-content: space-evenly;
+    flex-flow: row nowrap;
+    align-items: stretch;
+    flex-grow: 1;
+    text-align: left;
+    .kard {
+      flex-grow: 1;
+      border: 1px solid black;
+      background-color: $bg-white;
+      margin: 0.5vw;
+      img {
+        padding: 0.5em;
+      }
+      .kard-text {
+        padding: 0.5em;
+        h6 {
+          font-family: "Domine", serif;
+        }
+        p {
+          font-family: "Roboto", sans-serif;
+        }
+      }
+    }
+  }
+}
+#card-1 {
+  width: 50%;
+
+  .kard-images {
+    flex-grow: 1;
+    img {
+      display: flex;
+      width: 60em;
+      height: auto;
+    }
+  }
+  .kard-text {
+    align-self: flex-end;
+  }
+}
+#card-2 {
+  width: 50%;
+  img {
+    width: 30em;
+  }
+  .kard-text {
+    align-self: flex-end;
+  }
+}
+#card-3 {
+  width: 66%;
+  .kard-images {
+    justify-content: space-evenly;
+    width: 100%;
+    img {
+      max-width: 50%;
+    }
+  }
+  .kard-text {
+    margin: 0.5em;
+    align-self: flex-end;
+  }
+}
+#card-4 {
+  width: 34%;
+  flex-grow: 1;
+  .kard-images {
+    justify-content: space-evenly;
+    flex-grow: 1;
+    img {
+      max-width: 100%;
+    }
+  }
+  .kard-text {
+    margin: 0.5em;
+    align-self: flex-end;
+  }
+}
+
+#card-5 {
+  .flex-row {
+    justify-content: flex-end;
+    height: 100%;
+    align-items: flex-end;
+    .kard-text {
+      width: 30vw;
+      background-color: rgba($color: $bg-white, $alpha: 0.95);
+      padding: 0.5em;
+      h6 {
+        font-family: "Domine", serif;
+      }
+      p {
+        font-family: "Roboto", sans-serif;
+      }
+    }
+  }
+}
+.block {
+  display: flex; /* required */
+  flex-flow: row nowrap; /* required */
+}
+.block .background,
+.block .overlay {
+  box-sizing: border-box; /* required */
+  width: 100%; /* required */
+  flex: none; /* required */
+}
+
+.block .overlay {
+  margin-left: -100%; /* required */
+}
+
+#card-6,
+#card-7b {
+  .kard-images {
+    max-width: 20vw;
+  }
+}
+#card-8 {
+  width: 50%;
+  .kard-images {
+    img {
+      max-width: 50%;
+    }
+  }
+}
+#card-food {
+  background-color: $bg-white;
+  .background {
+    img {
+      z-index: 100;
+    }
+  }
+  .kard-text {
+    background-color: $bg-yellow;
+    width: 25vw;
+    z-index: 10;
+  }
+}
+
+#card-10,
+#card-12,
+#card-13 {
+  width: 34%;
+  justify-content: space-between;
+}
+
+#card-11,
+#card-14 {
+  width: 50%;
+  justify-content: space-between;
+}
+
+#card-2,
+#card-8,
+#card-10,
+#card-14 {
+  background-color: $bg-yellow;
+}
+#card-3,
+#card-7b,
+#card-11,
+#card-13 {
+  background-color: $bg-green;
+}
+</style>
