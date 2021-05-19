c7623f29e346af30c6f4df6ed7b320708e9d187b
Zui <zuik@users.noreply.github.com>
2021-05-18 23:40:13 -0400

    404 hack - part 2

--- src/main.js
+++ src/main.js
@@ -18,4 +18,11 @@ Vue.config.productionTip = false;
 new Vue({
   router,
   render: (h) => h(App),
+  created() {
+    if (sessionStorage.redirect) {
+      const redirect = sessionStorage.redirect;
+      delete sessionStorage.redirect;
+      this.$router.push(redirect);
+    }
+  },
 }).$mount("#app");
