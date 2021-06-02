51a1f71c2159e52ae8d9dac50eb7bed62b9063d0
Zui <zuik@users.noreply.github.com>
2021-06-01 23:31:28 -0400

    Update router page title

--- src/router/index.js
+++ src/router/index.js
@@ -15,24 +15,29 @@ const routes = [
     name: "Designs",
     component: () =>
       import(/* webpackChunkName: "designs" */ "../views/Designs.vue"),
+    meta: { title: "Alex -> Passion -> Design" },
   },
   {
     path: "/french",
     name: "French",
     component: () =>
       import(/* webpackChunkName: "french" */ "../views/French.vue"),
+    meta: { title: "Alex -> Passion -> French" },
   },
   {
     path: "/photography",
     name: "Photography",
     component: () =>
       import(/* webpackChunkName: "photography" */ "../views/Photography.vue"),
+    meta: { title: "Alex -> Passion -> Photography" },
   },
   {
     path: "/projects/playin_tune",
     name: "PlayinTune",
     component: () =>
       import(/* webpackChunkName: "playin_tune" */ "../views/PlayinTune.vue"),
+    meta: { title: "Alex -> UX Projects -> PlayinTune" },
+
     children: [
       {
         path: "mood-board",
@@ -41,6 +46,7 @@ const routes = [
           import(
             /* webpackChunkName: "playin_tune_mood_board" */ "../components/PlayinTuneMoodBoard.vue"
           ),
+        meta: { title: "Alex -> UX Projects -> PlayinTune -> Mood Board" },
       },
       {
         path: "nav-map",
@@ -49,6 +55,7 @@ const routes = [
           import(
             /* webpackChunkName: "playin_tune_nav_map" */ "../components/PlayinTuneNavMap.vue"
           ),
+        meta: { title: "Alex -> UX Projects -> PlayinTune -> Navigation Map" },
       },
       {
         path: "buttons",
@@ -57,6 +64,7 @@ const routes = [
           import(
             /* webpackChunkName: "playin_tune_buttons" */ "../components/PlayinTuneButtons.vue"
           ),
+        meta: { title: "Alex -> UX Projects -> PlayinTune -> Buttons" },
       },
       {
         path: "sketches",
@@ -65,6 +73,7 @@ const routes = [
           import(
             /* webpackChunkName: "playin_tune_sketches" */ "../components/PlayinTuneSketches.vue"
           ),
+        meta: { title: "Alex -> UX Projects -> PlayinTune -> Sketches" },
       },
     ],
   },
@@ -73,6 +82,7 @@ const routes = [
     name: "EcoSpark",
     component: () =>
       import(/* webpackChunkName: "eco_spark" */ "../views/EcoSpark.vue"),
+    meta: { title: "Alex -> UX Projects -> eco-Sparks" },
     children: [
       {
         path: "description",
@@ -81,6 +91,7 @@ const routes = [
           import(
             /* webpackChunkName: "eco_spark_description" */ "../components/EcoSparkDescription.vue"
           ),
+        meta: { title: "Alex -> UX Projects -> eco-Sparks -> Descriptions" },
       },
       {
         path: "site-map",
@@ -89,6 +100,9 @@ const routes = [
           import(
             /* webpackChunkName: "eco_spark_site_map" */ "../components/EcoSparkSiteMap.vue"
           ),
+        meta: {
+          title: "Alex -> UX Projects -> eco-Sparks -> Site Map & Wireframe",
+        },
       },
       {
         path: "mood-board",
@@ -97,6 +111,7 @@ const routes = [
           import(
             /* webpackChunkName: "eco_spark_mood_board" */ "../components/EcoSparkMoodBoard.vue"
           ),
+        meta: { title: "Alex -> UX Projects -> eco-Sparks -> Mood Board" },
       },
       {
         path: "prototype",
@@ -105,6 +120,7 @@ const routes = [
           import(
             /* webpackChunkName: "eco_spark_prototype" */ "../components/EcoSparkPrototype.vue"
           ),
+        meta: { title: "Alex -> UX Projects -> eco-Sparks -> Prototype" },
       },
       {
         path: "reflection",
@@ -113,6 +129,7 @@ const routes = [
           import(
             /* webpackChunkName: "eco_spark_reflection" */ "../components/EcoSparkReflection.vue"
           ),
+        meta: { title: "Alex -> UX Projects -> eco-Sparks -> Reflection" },
       },
     ],
   },
@@ -121,6 +138,7 @@ const routes = [
     name: "Volunteer",
     component: () =>
       import(/* webpackChunkName: "volunteer" */ "../views/Volunteer.vue"),
+    meta: { title: "Alex -> Volunteering" },
     children: [
       {
         path: "sps",
@@ -129,6 +147,7 @@ const routes = [
           import(
             /* webpackChunkName: "volunteer_sps" */ "../components/VolunteerSPS.vue"
           ),
+        meta: { title: "Alex -> Volunteering -> SPS" },
       },
       {
         path: "mymca/yes",
@@ -137,6 +156,7 @@ const routes = [
           import(
             /* webpackChunkName: "volunteer_mymca_yes" */ "../components/VolunteerMYMCAYes.vue"
           ),
+        meta: { title: "Alex -> Volunteering -> MYMCA -> Yes" },
       },
       {
         path: "mymca/prsc",
@@ -145,6 +165,7 @@ const routes = [
           import(
             /* webpackChunkName: "volunteer_mymca_prsc" */ "../components/VolunteerMYMCASummerCamp.vue"
           ),
+        meta: { title: "Alex -> Volunteering -> MYMCA -> PRSC" },
       },
       {
         path: "mymca/mentoring",
@@ -153,6 +174,7 @@ const routes = [
           import(
             /* webpackChunkName: "volunteer_mymca_mentoring" */ "../components/VolunteerMYMCAMentoring.vue"
           ),
+        meta: { title: "Alex -> Volunteering -> MYMCA -> M.Y. Mentoring" },
       },
     ],
   },
@@ -165,6 +187,14 @@ const router = new VueRouter({
   scrollBehavior() {
     return { x: 0, y: 0 };
   },
+});
+const DEFAULT_TITLE = "Alexandria Rodrigues";
+router.afterEach((to) => {
+  // Use next tick to handle router history correctly
+  // see: https://github.com/vuejs/vue-router/issues/914#issuecomment-384477609
+  Vue.nextTick(() => {
+    document.title = to.meta.title || DEFAULT_TITLE;
+  });
 });
 
 export default router;
